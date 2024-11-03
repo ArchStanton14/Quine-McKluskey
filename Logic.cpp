110bce88af34238ffd9e64f666e43e5eb8401d7f
Nathaniel Stanford <nathanieljstanford@gmail.com>
2024-11-02 21:08:36 -0400

    changed term storage to string vectors

--- Logic.cpp
+++ Logic.cpp
@@ -8,22 +8,34 @@ Logic::Logic(){
 *   @param expression the logical expression
 */
 Logic::Logic(std::string const &expression){
-
+    this->expression = expression;
 }
 
 /*
 * @return minterms of the function
 */
-std::unordered_set<int> Logic::getminterms() const{
-
+//std::unordered_set<int> Logic::getminterms() const{
+std::vector<std::string> Logic::getminterms() const{
+    return minterms;
 }
 
 /*
 * @return Maxterms of the function
 */
 
-std::unordered_set<int> Logic::getMaxterms() const{
+//std::unordered_set<int> Logic::getMaxterms() const{
+std::vector<std::string> Logic::getMaxterms() const{
+    return Maxterms;
+}
 
+
+/*
+* generates min and max terms from the expression
+* @post sets the minterms and maxterms
+*/
+
+void getTerms(){
+    
 }
 
 /*
@@ -32,11 +44,12 @@ std::unordered_set<int> Logic::getMaxterms() const{
 */
 
 void Logic::QM(){
-
+   //arrange by single bit differences
+   //look for additional bit differences 
 }
 
-
+/*
 std::unordered_set<int> minterms;   //the minterms of the function 
 std::unordered_set<int> Maxterms;   //the Maxterms of the function
 std::string expression; //the function expression
-
+*/
--- Logic.hpp
+++ Logic.hpp
@@ -3,6 +3,7 @@
 //declarations for Logic object for QM
 #include <unordered_set>//order doesn't matter
 #include <string>
+#include <vector>
 class Logic{
     public:
         /*Default constructor*/
@@ -15,13 +16,21 @@ class Logic{
         /*
         * @return minterms of the function
         */
-        std::unordered_set<int> getminterms() const;
-        
+        //std::unordered_set<int> getminterms() const;
+        std::vector<std::string> getminterms() const;
         /*
         * @return Maxterms of the function
         */
         
-        std::unordered_set<int> getMaxterms() const;
+        //std::unordered_set<int> getMaxterms() const;
+        std::vector<std::string> getMaxterms() const;
+        
+        /*
+        * generates min and max terms from the expression
+        * @post sets the minterms and maxterms
+        */
+
+        void getTerms();
 
         /*
         * uses Quine Mckluskey to simplify function
@@ -32,8 +41,10 @@ class Logic{
 
         //later add function to get/set notation and symbology
     private:
-        std::unordered_set<int> minterms;   //the minterms of the function 
-        std::unordered_set<int> Maxterms;   //the Maxterms of the function
+        //std::unordered_set<int> minterms;   //the minterms of the function (binary numbers)
+        //std::unordered_set<int> Maxterms;   //the Maxterms of the function (binary numbers)
+        std::vector<std::string> minterms; // the minterms of the function as binary strings
+        std::vector<std::string> Maxterms; // the Maxterms of the function as binary strings        
         std::string expression; //the function expression
         //later add member for infix/postfix/prefix notation
         //later add member for symbology (+,^,*,etc)
